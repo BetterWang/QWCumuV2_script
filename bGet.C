700fb2c4568d79e14907f8014de3777212c2c5da
Quan Wang <BetterWang@gmail.com>
2014-01-15 15:33:35 +0100

    local plot branch

---
+++ bGet.C
@@ -0,0 +1,79 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  bGet.C
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/13/2014 13:57:41
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+{
+	TH1D * hMult;
+	TH1D * hCent;
+	TH1D * hNoff;
+	TH1D * hQ22[20];
+	TH1D * hQ24[20];
+	TH1D * hQ26[20];
+	TH1D * hQ28[20];
+	TH1D * hQ22i[20];
+	TH1D * hQ24i[20];
+	TH1D * hQ26i[20];
+	TH1D * hQ28i[20];
+
+	TH1D * hC22[20];
+	TH1D * hC24[20];
+	TH1D * hC26[20];
+	TH1D * hC28[20];
+	TH1D * hV22[20];
+	TH1D * hV24[20];
+	TH1D * hV26[20];
+	TH1D * hV28[20];
+
+
+
+	hMult = (TH1D*)f->Get("hMult");
+	hCent = (TH1D*)f->Get("hCent");
+	hNoff = (TH1D*)f->Get("hNoff");
+
+	TH1D* hC22Cent = (TH1D*)f->Get("hC22Cent");
+	TH1D* hC24Cent = (TH1D*)f->Get("hC24Cent");
+	TH1D* hC26Cent = (TH1D*)f->Get("hC26Cent");
+	TH1D* hC28Cent = (TH1D*)f->Get("hC28Cent");
+
+	TH1D* hV22Cent = (TH1D*)f->Get("hV22Cent");
+	TH1D* hV24Cent = (TH1D*)f->Get("hV24Cent");
+	TH1D* hV26Cent = (TH1D*)f->Get("hV26Cent");
+	TH1D* hV28Cent = (TH1D*)f->Get("hV28Cent");
+
+	for ( int c = 0; c < 20; c++ ) {
+		hQ22[c] = (TH1D*) f->Get(Form("%s_%i", "hQ22", c));
+		hQ24[c] = (TH1D*) f->Get(Form("%s_%i", "hQ24", c));
+		hQ26[c] = (TH1D*) f->Get(Form("%s_%i", "hQ26", c));
+		hQ28[c] = (TH1D*) f->Get(Form("%s_%i", "hQ28", c));
+
+		hC22[c] = (TH1D*) f->Get(Form("%s_%i", "hC22", c));
+		hC24[c] = (TH1D*) f->Get(Form("%s_%i", "hC24", c));
+		hC26[c] = (TH1D*) f->Get(Form("%s_%i", "hC26", c));
+		hC28[c] = (TH1D*) f->Get(Form("%s_%i", "hC28", c));
+
+		hV22[c] = (TH1D*) f->Get(Form("%s_%i", "hV22", c));
+		hV24[c] = (TH1D*) f->Get(Form("%s_%i", "hV24", c));
+		hV26[c] = (TH1D*) f->Get(Form("%s_%i", "hV26", c));
+		hV28[c] = (TH1D*) f->Get(Form("%s_%i", "hV28", c));
+
+		hQ22i[c] = (TH1D*) f->Get(Form("%s_%i", "hQ22i", c));
+		hQ24i[c] = (TH1D*) f->Get(Form("%s_%i", "hQ24i", c));
+		hQ26i[c] = (TH1D*) f->Get(Form("%s_%i", "hQ26i", c));
+		hQ28i[c] = (TH1D*) f->Get(Form("%s_%i", "hQ28i", c));
+	}
+}
---
+++ plot.C
@@ -0,0 +1,166 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  plot.C
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/13/2014 12:56:16
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+{
+#include "label.h"
+
+	int s1 = 10;
+	int cent = 3;
+	int save = 1;
+
+#include "../../style.h"
+        SetStyle();
+	gStyle->SetOptTitle(0);
+
+	TFile * f = new TFile(Form("%s/output.root", ftxt[s1]));
+	gROOT->Macro("bGet.C");
+
+	TCanvas * cQ2 = new TCanvas("cQ2", "cQ2", 600, 600);
+	cQ2->SetGrid();
+	cQ2->SetLogy();
+	hQ22[cent]->SetTitle(";Q_{2}{2};Nevt");
+	hQ22[cent]->Draw();
+
+	TCanvas * cQ4 = new TCanvas("cQ4", "cQ4", 600, 600);
+	cQ4->SetGrid();
+	cQ4->SetLogy();
+	hQ24[cent]->SetTitle(";Q_{2}{4};Nevt");
+	hQ24[cent]->Draw();
+
+	TCanvas * cQ6 = new TCanvas("cQ6", "cQ6", 600, 600);
+	cQ6->SetGrid();
+	cQ6->SetLogy();
+	hQ26[cent]->SetTitle(";Q_{2}{6};Nevt");
+	hQ26[cent]->Draw();
+
+	TCanvas * cQ8 = new TCanvas("cQ8", "cQ8", 600, 600);
+	cQ8->SetGrid();
+	cQ8->SetLogy();
+	hQ28[cent]->SetTitle(";Q_{2}{8};Nevt");
+	hQ28[cent]->Draw();
+
+	TCanvas * cC2 = new TCanvas("cC2", "cC2", 600, 600);
+	cC2->SetGrid();
+	cC2->SetLogy();
+	hC22[cent]->SetTitle(";C_{2}{2};Nevt");
+	hC22[cent]->Draw();
+
+	TCanvas * cC4 = new TCanvas("cC4", "cC4", 600, 600);
+	cC4->SetGrid();
+	cC4->SetLogy();
+	hC24[cent]->SetTitle(";C_{2}{4};Nevt");
+	hC24[cent]->Draw();
+
+	TCanvas * cC6 = new TCanvas("cC6", "cC6", 600, 600);
+	cC6->SetGrid();
+	cC6->SetLogy();
+	hC26[cent]->SetTitle(";C_{2}{6};Nevt");
+	hC26[cent]->Draw();
+
+	TCanvas * cC8 = new TCanvas("cC8", "cC8", 600, 600);
+	cC8->SetGrid();
+	cC8->SetLogy();
+	hC28[cent]->SetTitle(";C_{2}{8};Nevt");
+	hC28[cent]->Draw();
+
+	TCanvas * cV2 = new TCanvas("cV2", "cV2", 600, 600);
+	cV2->SetGrid();
+	cV2->SetLogy();
+	hV22[cent]->SetTitle(";V_{2}{2};Nevt");
+	hV22[cent]->Draw();
+
+	TCanvas * cV4 = new TCanvas("cV4", "cV4", 600, 600);
+	cV4->SetGrid();
+	cV4->SetLogy();
+	hV24[cent]->SetTitle(";V_{2}{4};Nevt");
+	hV24[cent]->Draw();
+
+	TCanvas * cV6 = new TCanvas("cV6", "cV6", 600, 600);
+	cV6->SetGrid();
+	cV6->SetLogy();
+	hV26[cent]->SetTitle(";V_{2}{6};Nevt");
+	hV26[cent]->Draw();
+
+	TCanvas * cV8 = new TCanvas("cV8", "cV8", 600, 600);
+	cV8->SetGrid();
+	cV8->SetLogy();
+	hV28[cent]->SetTitle(";V_{2}{8};Nevt");
+	hV28[cent]->Draw();
+
+	TCanvas * cV2cent = new TCanvas("cV2cent", "cV2cent", 600, 600);
+	hV22Cent->SetTitle(";Cent;C22");
+	hV22Cent->Draw();
+	
+	TCanvas * cV4cent = new TCanvas("cV4cent", "cV4cent", 600, 600);
+	hV24Cent->SetTitle(";Cent;C24");
+	hV24Cent->Draw();
+
+	TCanvas * cV6cent = new TCanvas("cV6cent", "cV6cent", 600, 600);
+	hV26Cent->SetTitle(";Cent;C26");
+	hV26Cent->Draw();
+
+	TCanvas * cV8cent = new TCanvas("cV8cent", "cV8cent", 600, 600);
+	hV28Cent->SetTitle(";Cent;C28");
+	hV28Cent->Draw();
+
+	TCanvas * cC2cent = new TCanvas("cC2cent", "cC2cent", 600, 600);
+	hC22Cent->SetTitle(";Cent;C22");
+	hC22Cent->Draw();
+	
+	TCanvas * cC4cent = new TCanvas("cC4cent", "cC4cent", 600, 600);
+	hC24Cent->SetTitle(";Cent;C24");
+	hC24Cent->Draw();
+
+	TCanvas * cC6cent = new TCanvas("cC6cent", "cC6cent", 600, 600);
+	hC26Cent->SetTitle(";Cent;C26");
+	hC26Cent->Draw();
+
+	TCanvas * cC8cent = new TCanvas("cC8cent", "cC8cent", 600, 600);
+	hC28Cent->SetTitle(";Cent;C28");
+	hC28Cent->Draw();
+
+	if ( save ) {
+		cQ2->SaveAs(Form("cQ2_%i.png", cent));
+		cQ4->SaveAs(Form("cQ4_%i.png", cent));
+		cQ6->SaveAs(Form("cQ6_%i.png", cent));
+		cQ8->SaveAs(Form("cQ8_%i.png", cent));
+
+		cC2->SaveAs(Form("cC2_%i.png", cent));
+		cC4->SaveAs(Form("cC4_%i.png", cent));
+		cC6->SaveAs(Form("cC6_%i.png", cent));
+		cC8->SaveAs(Form("cC8_%i.png", cent));
+
+		cV2->SaveAs(Form("cV2_%i.png", cent));
+		cV4->SaveAs(Form("cV4_%i.png", cent));
+		cV6->SaveAs(Form("cV6_%i.png", cent));
+		cV8->SaveAs(Form("cV8_%i.png", cent));
+
+		cC2cent->SaveAs("cC2cent.png");
+		cC4cent->SaveAs("cC4cent.png");
+		cC6cent->SaveAs("cC6cent.png");
+		cC8cent->SaveAs("cC8cent.png");
+
+		cV2cent->SaveAs("cV2cent.png");
+		cV4cent->SaveAs("cV4cent.png");
+		cV6cent->SaveAs("cV6cent.png");
+		cV8cent->SaveAs("cV8cent.png");
+	}
+
+
+}
